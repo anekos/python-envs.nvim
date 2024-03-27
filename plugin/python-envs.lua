2dccb9c11d0a9b90bc0fa8c2ea49e267e0e91d27
anekos <anekos@snca.net>
2024-03-27 16:29:20 +0900

    Add `PythonPath` command

--- plugin/python-envs.lua
+++ plugin/python-envs.lua
@@ -2,35 +2,33 @@ local au_group = vim.api.nvim_create_augroup('anekos-python-envs', { clear = tru
 
 local events = require('python-envs.events')
 
-vim.api.nvim_create_autocmd(
-  {'VimEnter'},
-  {
-    pattern = '*',
-    callback = function()
-      events.init(vim.env.PWD)
-    end,
-    group = au_group,
-  }
-)
+vim.api.nvim_create_autocmd({ 'VimEnter' }, {
+  pattern = '*',
+  callback = function()
+    events.init(vim.env.PWD)
+  end,
+  group = au_group,
+})
 
-vim.api.nvim_create_autocmd(
-  {'DirChanged'},
-  {
-    pattern = '*',
-    callback = function(ev)
-      events.init(ev.file)
-    end,
-    group = au_group,
-  }
-)
+vim.api.nvim_create_autocmd({ 'DirChanged' }, {
+  pattern = '*',
+  callback = function(ev)
+    events.init(ev.file)
+  end,
+  group = au_group,
+})
 
-vim.api.nvim_create_autocmd(
-  {'DirChangedPre'},
-  {
-    pattern = '*',
-    callback = function()
-      events.cleanup()
-    end,
-    group = au_group,
-  }
-)
+vim.api.nvim_create_autocmd({ 'DirChangedPre' }, {
+  pattern = '*',
+  callback = function()
+    events.cleanup()
+  end,
+  group = au_group,
+})
+
+vim.api.nvim_create_user_command('PythonPath', function(args)
+  events.init(args.args)
+end, {
+  nargs = 1,
+  complete = 'dir',
+})
